cf319476b41317cdd99b686153dfc7ec3eada800
aknight61571 <aleck@opioidcm.com>
2025-05-16 12:00:03 -0500

    Reworded third point in Summary

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ st.markdown("""
     </div>
     <div class="summary-item">1. Opioid use causes stress, which degrades physical health and productivity. Construction workers are much more likely to use opioids.</div>
     <div class="summary-item">2. Interruption in OpioidRx-AI service increases frequency of opioid use among employees.</div>
-    <div class="summary-item">3. Employees first identified by OpioidRx-AI are expensive to insure. OPCM drastically lowers costs by the next quarter.</div>
+    <div class="summary-item">3. Employees identified by OpioidRx-AI are expensive to insure. OPCM drastically lowers costs by the next quarter.</div>
 </div>
 """, unsafe_allow_html=True)
 
