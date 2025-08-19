432a770a66a8f61022d7f700f38e2c2025ecb57c
aknight61571 <aleck@opioidcm.com>
2025-08-19 14:29:37 -0500

    Update regression_report.py

--- regression_report.py
+++ regression_report.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.io as pio
 import numpy as np
 
-st.cache_data_clear()
+st.cache_data.clear()
 
 # ─── Page Config ──────────────────────────────────────────────────────────────
 st.set_page_config(layout='wide', page_title='Opioid Cost Report', initial_sidebar_state='collapsed')
@@ -350,6 +350,7 @@ with col3:
 with col4:
     fig_b = pio.read_json('reg_cost_pmem.json')
     st.plotly_chart(fig_b, use_container_width=True)
+
 
 
 
