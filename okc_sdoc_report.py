51b8d881193faad4fdf36e90ab442125317a5b30
aknight61571 <aleck@opioidcm.com>
2025-09-30 12:02:18 -0500

    Revert before helper funcs

--- okc_sdoc_report.py
+++ okc_sdoc_report.py
@@ -87,28 +87,17 @@ st.write("Update this text later")
 # --- FOUR BOXES ---
 colA, colB, colC, colD = st.columns(4)
 
-def img_to_bytes(img_path):
-    """Convert an image file to Base64 encoded bytes."""
-    img_bytes = Path(img_path).read_bytes()
-    encoded = base64.b64encode(img_bytes).decode()
-    return encoded
-
-def img_to_html(img_path):
-    """Create an HTML image tag with Base64 encoded image data."""
-    img_html = f"<img src='data:image/png;base64,{img_to_bytes(img_path)}' />"
-    return img_html
-
-img_src = img_to_html("gray_pill_bottle.png")
 with colA:
     st.markdown(
-        f"""
+        """
         <div class="box">
-            {img_src}
+            <img src="gray_pill_bottle.png" />
             <div class="box-text">50+ mg Morphine Equivalent</div>
         </div>
         """,
         unsafe_allow_html=True,
     )
+
 with colB:
     st.markdown(
         """
