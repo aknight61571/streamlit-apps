9cc595d20416bf6609e2a38a0cc9a037c498c8d7
aknight61571 <aleck@opioidcm.com>
2025-09-30 11:31:59 -0500

    add png reader func

--- okc_sdoc_report.py
+++ okc_sdoc_report.py
@@ -87,6 +87,17 @@ st.write("Update this text later")
 # --- FOUR BOXES ---
 colA, colB, colC, colD = st.columns(4)
 
+def img_to_bytes(img_path):
+    """Convert an image file to Base64 encoded bytes."""
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+
+def img_to_html(img_path):
+    """Create an HTML image tag with Base64 encoded image data."""
+    img_html = f"<img src='data:image/png;base64,{img_to_bytes(img_path)}' />"
+    return img_html
+    
 with colA:
     st.markdown(
         """
