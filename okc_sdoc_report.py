f7aadac3c21718995ed80e03dd2656e495ed703f
aknight61571 <aleck@opioidcm.com>
2025-09-30 13:57:30 -0500

    Update okc_sdoc_report.py

--- okc_sdoc_report.py
+++ okc_sdoc_report.py
@@ -83,10 +83,6 @@ with col2:
 # --- FIRST TEXT SECTION ---
 st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("<div class='section-header'>What Are OPCM Indicators?</div>", unsafe_allow_html=True)
-st.write(
-    """OPCM's innovative <i>OpioidRx-AI</i> model is trained to evaluate opioid-related risk based on CDC-researched indicators<br>
-Examples of indicators include:"""
-)
 
 # --- FOUR BOXES ---
 colA, colB, colC, colD = st.columns(4)
@@ -154,3 +150,7 @@ with colD:
         """,
         unsafe_allow_html=True,
     )
+st.write(
+    """OPCM's innovative <i>OpioidRx-AI</i> model is trained to evaluate opioid-related risk based on CDC-researched indicators<br>
+Examples of indicators include:"""
+)
