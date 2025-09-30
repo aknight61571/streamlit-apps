fdb02342b3b693946d987a61e440da9759e64e2d
aknight61571 <aleck@opioidcm.com>
2025-09-30 11:21:17 -0500

    Icon sizing

--- okc_sdoc_report.py
+++ okc_sdoc_report.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Set page config
 st.set_page_config(page_title="Marketing Report", layout="wide")
 
-# Apply dark theme background color
+# Apply dark theme
 st.markdown(
     """
     <style>
@@ -23,20 +23,31 @@ st.markdown(
             margin-bottom: 5px;
         }
         .box {
-            background-color: #1E1E1E;
+            background-color: #4A4A4A; /* medium-light grey */
             border-radius: 12px;
             padding: 20px;
             text-align: center;
             box-shadow: 0px 2px 6px rgba(0,0,0,0.5);
+            height: 100%;
+        }
+        .box img {
+            max-width: 80px;  /* uniform scaling */
+            height: auto;
+        }
+        .box .combo {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 10px;
         }
         .box-text {
             font-weight: bold;
-            margin-top: 10px;
+            margin-top: 12px;
+            font-size: 16px;
         }
         .plus-sign {
-            font-size: 32px;
+            font-size: 28px;
             font-weight: bold;
-            margin: 0px 10px;
         }
     </style>
     """,
@@ -63,7 +74,7 @@ with col2:
     st.image("green_logo.png", use_container_width=True)
 
 # --- FIRST TEXT SECTION ---
-st.markdown("<br>", unsafe_allow_html=True)  # spacing
+st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("<div class='section-header'>What Are OPCM Indicators?</div>", unsafe_allow_html=True)
 st.write("Update this text later")
 
@@ -71,43 +82,57 @@ st.write("Update this text later")
 colA, colB, colC, colD = st.columns(4)
 
 with colA:
-    st.markdown("<div class='box'>", unsafe_allow_html=True)
-    st.image("gray_pill_bottle.png", use_container_width=True)
-    st.markdown("<div class='box-text'>50+ mg Morphine Equivalent</div>", unsafe_allow_html=True)
-    st.markdown("</div>", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div class="box">
+            <img src="gray_pill_bottle.png" />
+            <div class="box-text">50+ mg Morphine Equivalent</div>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
 
 with colB:
-    st.markdown("<div class='box'>", unsafe_allow_html=True)
-    img_cols = st.columns([1, 0.2, 1])
-    with img_cols[0]:
-        st.image("gray_pill_bottle.png", use_container_width=True)
-    with img_cols[1]:
-        st.markdown("<div class='plus-sign'>+</div>", unsafe_allow_html=True)
-    with img_cols[2]:
-        st.image("gray_pill_bottle.png", use_container_width=True)
-    st.markdown("<div class='box-text'>Opioid + Benzo</div>", unsafe_allow_html=True)
-    st.markdown("</div>", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div class="box">
+            <div class="combo">
+                <img src="gray_pill_bottle.png" />
+                <div class="plus-sign">+</div>
+                <img src="gray_pill_bottle.png" />
+            </div>
+            <div class="box-text">Opioid + Benzo</div>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
 
 with colC:
-    st.markdown("<div class='box'>", unsafe_allow_html=True)
-    img_cols = st.columns([1, 0.2, 1])
-    with img_cols[0]:
-        st.image("gray_pill_bottle.png", use_container_width=True)
-    with img_cols[1]:
-        st.markdown("<div class='plus-sign'>+</div>", unsafe_allow_html=True)
-    with img_cols[2]:
-        st.image("gray_doctor.png", use_container_width=True)
-    st.markdown("<div class='box-text'>2+ Opioid Prescribers</div>", unsafe_allow_html=True)
-    st.markdown("</div>", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div class="box">
+            <div class="combo">
+                <img src="gray_pill_bottle.png" />
+                <div class="plus-sign">+</div>
+                <img src="gray_doctor.png" />
+            </div>
+            <div class="box-text">2+ Opioid Prescribers</div>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
 
 with colD:
-    st.markdown("<div class='box'>", unsafe_allow_html=True)
-    img_cols = st.columns([1, 0.2, 1])
-    with img_cols[0]:
-        st.image("gray_pill_bottle.png", use_container_width=True)
-    with img_cols[1]:
-        st.markdown("<div class='plus-sign'>+</div>", unsafe_allow_html=True)
-    with img_cols[2]:
-        st.image("gray_pharmacy.png", use_container_width=True)
-    st.markdown("<div class='box-text'>2+ Opioid Pharmacies</div>", unsafe_allow_html=True)
-    st.markdown("</div>", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div class="box">
+            <div class="combo">
+                <img src="gray_pill_bottle.png" />
+                <div class="plus-sign">+</div>
+                <img src="gray_pharmacy.png" />
+            </div>
+            <div class="box-text">2+ Opioid Pharmacies</div>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
