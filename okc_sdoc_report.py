9a25469572ffde59c6bbcd1dd72cc7371ef9554a
aknight61571 <aleck@opioidcm.com>
2025-10-01 09:16:43 -0500

    Sec2 Format

--- okc_sdoc_report.py
+++ okc_sdoc_report.py
@@ -166,8 +166,9 @@ with col2:
     st.image("okc_cost_of_ident.png", use_container_width=True)
 # Center the SVG at 90% width
 col1, col2, col3 = st.columns([5, 90, 5])
+
+st.header("OPCM Outreach")
 with col2:
     st.image("Flowchart.svg", use_container_width=True)
 
-st.header("OPCM Outreach")
 st.write("Update this later")
