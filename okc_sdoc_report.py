e17c148753cdfb113c13aff6ec13ae3b7f939a92
aknight61571 <aleck@opioidcm.com>
2025-10-02 15:55:09 -0500

    text for outreach

--- okc_sdoc_report.py
+++ okc_sdoc_report.py
@@ -173,7 +173,12 @@ col1, col2, col3 = st.columns([5, 90, 5])
 with col2:
     st.image("Flowchart.svg", use_container_width=True)
 
-st.write("Update this later")
+st.write("""All prescribers who wrote an opioid Rx for a plan member identified by OpioidRx-Ai receive communication from OPCM pharmacists.
+<li>Prescribers for members with <90 continuous days of an opioid recieve a letter stating the employer has adopted CDC prescribing guidelines for opioid prescriptions.
+<li> Prescribers for members with >90 continuous days of an opioid recieve a personal call in addition to this letter, informing them of current CDC guidelines and verifying their intent to adhere.
+
+The quality of OPCM's outreach ensures that a large portion of high-risk cohorts are considered low-risk by the following fiscal quarter..
+""")
 
 col1, col2 = st.columns([67, 33])
 with col1:
