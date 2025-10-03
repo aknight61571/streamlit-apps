17de04c3ab56a98148dfc4bc8b84fc585a5a10e8
aknight61571 <aleck@opioidcm.com>
2025-10-03 14:07:00 -0500

    Update okc_sdoc_report.py

--- okc_sdoc_report.py
+++ okc_sdoc_report.py
@@ -214,9 +214,9 @@ col1, col2 = st.columns([67, 33])
 with col1:
     st.image('qident_qafter.png', use_container_width=True)
     st.write(
-        """How are 12% considered outliers? 
-        <i>Standard outliers are computed using the mean, which is subject to extreme outliers.</i><br>
-        Computing outliers with percentiles ensures that large jumps from the norm are always noted
+        """<b>How are 12% considered outliers?</b> 
+        <li>Standard outliers are computed using the mean, which is subject to extreme outliers.
+        <li>Computing outliers with percentiles ensures that large jumps from the norm are always noted.
         """,
         unsafe_allow_html=True
     )
